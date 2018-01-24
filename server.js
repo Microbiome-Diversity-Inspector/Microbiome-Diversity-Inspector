d606bb901df24d253d5c6996df2f6859756e0c40
Rachit Belwariar <gbelwariar@gmail.com>
2018-01-24 19:27:25 +0530

    Follow Google's JS styleguide as suggested by ESLint.

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ var countOfA, countOfT, countOfG, countOfC, first, last, size, fileName;
 
 app.use(express.static(path.join(__dirname, '')));
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 
 app.get('/', function( req, res ) {
 	res.sendFile(path.join(__dirname, 'tool.html'));
-});	
-	
+});
+
 // TODO: Comply with REST standards. POST calls are meant to alter the system whereas none
 // of the POST request is altering the system due to the intentional "lack" of database in
 // this project.
@@ -24,32 +24,32 @@ app.post('/analyze', function(req, res) {
 	countOfA = 0, countOfT = 0, countOfG = 0, countOfC = 0, first = 0, last = 499, size = 500;
 	fileName = path.join(__dirname, req.body.name);
 	res.send('Posting done');
-});		
-		
-		
-app.get('/analyze', function (req, res) {
-	var stat = fs.statSync(fileName);	
-	if (first > stat.size) { 
+});
+
+
+app.get('/analyze', function(req, res) {
+	let stat = fs.statSync(fileName);
+	if (first > stat.size) {
 		 var countObj = {
 				 countOfA: countOfA,
 				 countOfT: countOfT,
 				 countOfG: countOfG,
-				 countOfC: countOfC
-		 };	 
+				 countOfC: countOfC,
+		 };
 		 res.send({
 			 statusCode: 'x',			// 'x' denotes that this is the last response.
-			 countObj: countObj
+			 countObj: countObj,
 		 });
 		 console.log('Finished analyzing - ' + fileName);
-	} else {					
+	} else {
 			if (last > stat.size) {
 				last = stat.size - 1;
-			}	
-			var readStream = fs.createReadStream(fileName, {start: first, end: last});
-			readStream.on("data", function(data) {
-				for (var i=0; i<data.toString().length; i++) {
+			}
+			let readStream = fs.createReadStream(fileName, {start: first, end: last});
+			readStream.on('data', function(data) {
+				for (let i=0; i<data.toString().length; i++) {
 					// TODO: Exclude unneccessary characters taken into calculations.
-					switch(data.toString()[i]) {
+					switch (data.toString()[i]) {
 						case 'A':
 							countOfA++;
 							break;
@@ -64,36 +64,36 @@ app.get('/analyze', function (req, res) {
 							break;
 					}
 				}
-			});		
+			});
 			first += size;
 			last += size;
 			var countObj = {
 				countOfA: countOfA,
 				countOfT: countOfT,
 				countOfG: countOfG,
-				countOfC: countOfC
+				countOfC: countOfC,
 			};
 			res.send({
 				statusCode: 'o',			// 'o' denotes that this is not the last response.
-				countObj: countObj
+				countObj: countObj,
 			});
 	}
-})
+});
 
 
 app.post('/convertToFasta', function(req, res) {
-	var fastqFileName = path.join(__dirname, req.body.name);
-	var readStream = fs.createReadStream(fastqFileName);
+	let fastqFileName = path.join(__dirname, req.body.name);
+	let readStream = fs.createReadStream(fastqFileName);
 	// The line limit in FASTA format as mentioned in - https://en.wikipedia.org/wiki/FASTA_format
-	var FASTA_LINE_LIMIT = 60;
-	var currentCharacterCount = 1;
-	var fastaContent = '';
-	var isFirstLineOfSequence = true;
-	var isSecondLineOfSequence = false;
-	// The below code assumes that the file uploaded is a proper FASTQ file 
+	let FASTA_LINE_LIMIT = 60;
+	let currentCharacterCount = 1;
+	let fastaContent = '';
+	let isFirstLineOfSequence = true;
+	let isSecondLineOfSequence = false;
+	// The below code assumes that the file uploaded is a proper FASTQ file
 	// following the rules as mentioned in - https://en.wikipedia.org/wiki/FASTQ_format
-	readStream.on("data", function(data) {
-		for (var i=0; i<data.toString().length; i++) {
+	readStream.on('data', function(data) {
+		for (let i=0; i<data.toString().length; i++) {
 			// This is the first line of the sequence.
 			if (i === 0
 					|| (data.toString()[i-1] === '\n' && data.toString()[i] === '@' && isFirstLineOfSequence === false)) {
@@ -111,7 +111,7 @@ app.post('/convertToFasta', function(req, res) {
 				isSecondLineOfSequence = false;
 				isFirstLineOfSequence = true;
 			} else if ((i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true)
-									|| (i !== 0 && data.toString()[i-1] !== '\n' && isSecondLineOfSequence === true)) { 
+									|| (i !== 0 && data.toString()[i-1] !== '\n' && isSecondLineOfSequence === true)) {
 				// This is the second line of the sequence in which the nucelotide data is present.
 				if (i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true) {
 					// Reset the character count counter.
@@ -121,7 +121,7 @@ app.post('/convertToFasta', function(req, res) {
 				isSecondLineOfSequence = true;
 				if (currentCharacterCount < FASTA_LINE_LIMIT && data.toString()[i] !== '\r') {
 					fastaContent += data.toString()[i];
-					currentCharacterCount++;	
+					currentCharacterCount++;
 				} else if (currentCharacterCount === FASTA_LINE_LIMIT && data.toString()[i] !== '\r') {
 					currentCharacterCount = 1;
 					fastaContent += data.toString()[i];
@@ -133,27 +133,27 @@ app.post('/convertToFasta', function(req, res) {
 				isSecondLineOfSequence = false;
 			}
 		}
-	});		
-	readStream.on('end', () => {
+	});
+	readStream.on('end', function() {
 		console.log('Finished converting - ' + req.body.name + ' to its FASTA equivalent');
 		res.send(fastaContent);
-	});	
-});	
+	});
+});
 
 
 app.post('/convertToFastq', function(req, res) {
-	var fastaFileName = path.join(__dirname, req.body.fastaFileName);
-	var qualFileName = path.join(__dirname, req.body.qualFileName);
+	let fastaFileName = path.join(__dirname, req.body.fastaFileName);
+	let qualFileName = path.join(__dirname, req.body.qualFileName);
 	// Parse the FASTA file.
 	// If 'isFirstLineOfSequence' is set to false then it automatically means that the current
 	// line is the second line of the sequence having the nucelotide sequence.
-	var isFirstLineOfSequence = true;
-	var currentReadName = '';
-	var currentBase = '';
-	var readNameToBaseMap = {};
-	var fastaReadStream = fs.createReadStream(fastaFileName);
-	fastaReadStream.on("data", function(data) {
-		for (var i=0; i<data.toString().length; i++) {
+	let isFirstLineOfSequence = true;
+	let currentReadName = '';
+	let currentBase = '';
+	let readNameToBaseMap = {};
+	let fastaReadStream = fs.createReadStream(fastaFileName);
+	fastaReadStream.on('data', function(data) {
+		for (let i=0; i<data.toString().length; i++) {
 			// This is the first line of the sequence.
 			if (i === 0
 					|| (data.toString()[i-1] === '\n' && data.toString()[i] === '>' && isFirstLineOfSequence === false)) {
@@ -169,7 +169,7 @@ app.post('/convertToFastq', function(req, res) {
 				}
 				isFirstLineOfSequence = true;
 			} else if ((i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true)
-									|| isFirstLineOfSequence === false) { 
+									|| isFirstLineOfSequence === false) {
 				// This is the second and the last line of the sequence in which the nucelotide data is present.
 				if (i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true) {
 					// Reset the current base.
@@ -181,24 +181,24 @@ app.post('/convertToFastq', function(req, res) {
 				}
 			}
 		}
-	});		
-	fastaReadStream.on('end', () => {
+	});
+	fastaReadStream.on('end', function() {
 		if (currentReadName !== '') {
 			readNameToBaseMap[currentReadName] = currentBase;
 		}
-	});		
+	});
 
 	// Parse the QUAL file.
 	isFirstLineOfSequence = true;
 	currentReadName = '';
 	// See - http://biopython.org/DIST/docs/api/Bio.SeqIO.QualityIO-module.html to know more about
 	// Sanger-styled qualities.
-	var currentSangerStyledQualities = '';
-	var currentDecimalQualityInStringFormat = '';
-	var readNameToSangerStyledQualityMap = {};
-	var qualReadStream = fs.createReadStream(qualFileName);
-	qualReadStream.on("data", function(data) {
-		for (var i=0; i<data.toString().length; i++) {
+	let currentSangerStyledQualities = '';
+	let currentDecimalQualityInStringFormat = '';
+	let readNameToSangerStyledQualityMap = {};
+	let qualReadStream = fs.createReadStream(qualFileName);
+	qualReadStream.on('data', function(data) {
+		for (let i=0; i<data.toString().length; i++) {
 			// This is the first line of the sequence.
 			if (i === 0
 					|| (data.toString()[i-1] === '\n' && data.toString()[i] === '>' && isFirstLineOfSequence === false)) {
@@ -214,7 +214,7 @@ app.post('/convertToFastq', function(req, res) {
 				}
 				isFirstLineOfSequence = true;
 			} else if ((i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true)
-									|| isFirstLineOfSequence === false) { 
+									|| isFirstLineOfSequence === false) {
 				// This is the second and the last line of the sequence in which the nucelotide data is present.
 				if (i !== 0 && data.toString()[i-1] === '\n' && isFirstLineOfSequence === true) {
 					// Reset the current Sanger-styled quality.
@@ -225,43 +225,43 @@ app.post('/convertToFastq', function(req, res) {
 				if (data.toString()[i] !== ' ' && data.toString()[i] !== '\n' && data.toString()[i] !== '\r') {
 					currentDecimalQualityInStringFormat += data.toString()[i];
 				} else if (data.toString()[i] === ' ' || data.toString()[i] === '\n' || data.toString()[i] === '\r') {
-					// Don't add the condition to check again '\r' since in Windows it will 
-					// just lead to computing the Sanger-styled quality twice of the same 
+					// Don't add the condition to check again '\r' since in Windows it will
+					// just lead to computing the Sanger-styled quality twice of the same
 					// number as '\r' and '\n' always occurs in pair in Windows.
 					// See this for more - https://stackoverflow.com/a/1761086/5928129.
 					// The second condition in the above OR statement is there to separate
 					// two quality numbers separated just by a newline.
 					if (currentDecimalQualityInStringFormat !== '') {
 						// The above 'if-guard' is to prevent the conversion of an empty decimal
-						// quality into its Sanger-styled counterpart if it has already been 
+						// quality into its Sanger-styled counterpart if it has already been
 						// reset to empty on an encounter of a previous ' ' character.
 						currentSangerStyledQualities =
 								currentSangerStyledQualities +
 								convertDecimalQualityInStringFormatToSangerStyledQuality(currentDecimalQualityInStringFormat);
-						currentDecimalQualityInStringFormat = '';						
+						currentDecimalQualityInStringFormat = '';
 					}
 				}
 			}
 		}
-	});	
-	qualReadStream.on('end', () => {
+	});
+	qualReadStream.on('end', function() {
 		if (currentReadName !== '') {
 			if (currentDecimalQualityInStringFormat != '') {
 				currentSangerStyledQualities =
 						currentSangerStyledQualities +
-						convertDecimalQualityInStringFormatToSangerStyledQuality(currentDecimalQualityInStringFormat);							
+						convertDecimalQualityInStringFormatToSangerStyledQuality(currentDecimalQualityInStringFormat);
 			}
 			readNameToSangerStyledQualityMap[currentReadName] = currentSangerStyledQualities;
 		}
-		var fastqContent = '';
-		for (var key in readNameToBaseMap) {
+		let fastqContent = '';
+		for (let key in readNameToBaseMap) {
 			if (readNameToBaseMap.hasOwnProperty(key)) {
 				fastqContent += '@';
 				fastqContent += key;
-				// We don't add a '\r' before adding '\n' since just adding a '\n' is compatible 
+				// We don't add a '\r' before adding '\n' since just adding a '\n' is compatible
 				// with both Windows and Unix-based systems whereas in Unix a '\r' does not signify anything.
 				// See - https://stackoverflow.com/a/1761086/5928129 for more.
-				fastqContent += '\n';	
+				fastqContent += '\n';
 				fastqContent += readNameToBaseMap[key];
 				fastqContent += '\n';
 				fastqContent += '+';
@@ -269,25 +269,25 @@ app.post('/convertToFastq', function(req, res) {
 				fastqContent += readNameToSangerStyledQualityMap[key];
 				fastqContent += '\n';
 			}
-		}			
+		}
 		console.log('Finished converting - ' + req.body.fastaFileName + ' to its FASTA equivalent');
 		res.send(fastqContent);
-	});		
-});	
+	});
+});
 
 
 /**
- * A private function to map a decimal quality to Sanger-styled quality 
- * using the formula - chr(Q+33) as mentioned in - 
+ * A private function to map a decimal quality to Sanger-styled quality
+ * using the formula - chr(Q+33) as mentioned in -
  * http://biopython.org/DIST/docs/api/Bio.SeqIO.QualityIO-module.html
  */
 function convertDecimalQualityInStringFormatToSangerStyledQuality(input) {
-	var qualityInDecimalFormat = parseInt(input, 10);
+	let qualityInDecimalFormat = parseInt(input, 10);
 	return String.fromCharCode(qualityInDecimalFormat + 33);
 }
 
-var server = app.listen(8080, function () {
-	var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://%s:%s", host, port)
-})
+var server = app.listen(8080, function() {
+	let host = server.address().address;
+  let port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
