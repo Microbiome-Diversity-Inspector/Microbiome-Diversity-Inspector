59aceeadab9ba0606294cc279e4a8322b97d370b
Rachit Belwariar <gbelwariar@gmail.com>
2018-01-26 11:07:33 +0530

    This commit does the followint things-
    1) Provide the client-side code corresponding to this PR - https://github.com/gbelwariar/Microbiome-Diversity-Inspector/pull/39 &
    https://github.com/gbelwariar/Microbiome-Diversity-Inspector/pull/40
    2) Follow Google JS style guide.
    
    Note that to obtain the list of samthe AJAX request to One Codex REST API is made in the client itself since it is expected to get the response quickly.

--- client.js
+++ client.js
@@ -1,20 +1,23 @@
-function MyController($document, $http, $interval) {
+function MyController($scope, $document, $http, $interval) {
 	this.selectedCityUrlPrefix;
 	this.accessionNumber;
+	this.apiKey;
+	this.samples;
 	this.$onInit = (function() {
 		this.cities = [
 		{
 			name: 'New York',
-			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/NY/data/'
+			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/NY/data/',
 		},
 		{
 			name: 'Boston',
-			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/Boston/data/'
+			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/Boston/data/',
 		},
 		{
 			name: 'Sacramento',
-			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/Sacramento/data/'
-		}];		
+			urlPrefix: 'http://ala.boku.ac.at/camda2017/MetaSUB/Sacramento/data/',
+		}];
+		this.scope_ = $scope;
 		this.intervalService_ = $interval;
 		this.httpService_ = $http;
 		this.document_ = $document;
@@ -22,12 +25,12 @@ function MyController($document, $http, $interval) {
 		this.qualFileName;		// Setting on controller to share the file name with FASTA file uploader.
 		this.reset_();
 	}).bind(this);
-}		
+}
 
 
 function getLogOfXBase2(x) {
 	return Math.log(x) / Math.log(2);
-}				
+}
 
 
 function isFileHavingCorrectFormat(fileName, expectedFileFormat) {
@@ -41,57 +44,57 @@ function isFileHavingCorrectFormat(fileName, expectedFileFormat) {
  *  See - https://stackoverflow.com/a/18197341/5928129 for more.
  */
 function download(filename, text) {
-	var element = document.createElement('a');
-	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-	element.setAttribute('download', filename);
-	element.style.display = 'none';
-	document.body.appendChild(element);
-	element.click();
-	document.body.removeChild(element);
-}				
+	let elem = document.createElement('a');
+	elem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	elem.setAttribute('download', filename);
+	elem.style.display = 'none';
+	document.body.appendChild(elem);
+	elem.click();
+	document.body.removeChild(elem);
+}
 
 
 /**
  * Refresh the graph between entropy of partitioned blocks of DNA and time.
- * 
+ *
  * @private
- */	
+ */
 MyController.prototype.refreshEntropyGraph_ = function() {
-		var dps = []; // dataPoints
-		var chart = new CanvasJS.Chart("chartContainer", {
-			title :{
-				text: "Entropy (of continuous chunks)   vs   Time",
-				fontColor: "#2f4f4f",
+		let dps = []; // dataPoints
+		let chart = new CanvasJS.Chart('chartContainer', {
+			title: {
+				text: 'Entropy (of continuous chunks)   vs   Time',
+				fontColor: '#2f4f4f',
 				fontSize: 30,
 				padding: 10,
 				margin: 15,
-				backgroundColor: "#FFFFE0",
+				backgroundColor: '#FFFFE0',
 				borderThickness: 1,
-				cornerRadius: 5						
-			},     
+				cornerRadius: 5,						
+			},
 			axisY: {
-				includeZero: false
-			},      						
+				includeZero: false,
+			},
 			data: [{
-				type: "line",
-				dataPoints: dps
-			}]
+				type: 'line',
+				dataPoints: dps,
+			}],
 		});
-		var xVal = 0;
-		var updateInterval = 500;
-		var dataLength = 20; // number of dataPoints visible at any point
+		let xVal = 0;
+		let updateInterval = 500;
+		let dataLength = 20; // number of dataPoints visible at any point
 
-		var updateGraph = (function (count) {
+		let updateGraph = (function(count) {
 			// Stop updating the chart once the whole file is analyzed.
 			if (this.showEntropy === true) {
 				return;
 			}
 			count = count || 1;
-			//debugger;
-			for (var j = 0; j < count; j++) {
+			// debugger;
+			for (let j = 0; j < count; j++) {
 				dps.push({
 					x: xVal,
-					y: this.entropyOfCurrentWindow
+					y: this.entropyOfCurrentWindow,
 				});
 				xVal++;
 			}
@@ -101,31 +104,33 @@ MyController.prototype.refreshEntropyGraph_ = function() {
 			chart.render();
 		}).bind(this);
 		updateGraph(dataLength);
-		setInterval(function(){updateGraph()}, updateInterval);
-}
+		setInterval(function() {
+updateGraph()
+}, updateInterval);
+};
 
 
 /**
  * Returns the entropy from the counts of adenine (A), cytosine (C),
  * guanine (G), thymine (T), and uracil (U).
- * 
+ *
  * @private
  * @param {number} countOfA The count of Adenine base.
  * @param {number} countOfT The count of Thymine base.
  * @param {number} countOfG The count of Guanine base.
- * @param {number} countOfC The count of Cytosine base.			 
+ * @param {number} countOfC The count of Cytosine base.
  * @return {number}
- */				
+ */
 MyController.prototype.getEntropy_ = function(countOfA, countOfT, countOfG, countOfC) {
-	var totalCount = countOfA + countOfT + countOfG + countOfC;
+	let totalCount = countOfA + countOfT + countOfG + countOfC;
 	if (totalCount === 0) {
 		return 0;
 	}
-	var probabilityOfA = countOfA / totalCount;
-	var probabilityOfT = countOfT / totalCount;
-	var probabilityOfG = countOfG / totalCount;
-	var probabilityOfC = countOfC / totalCount;
-	var entropy = -1 * (
+	let probabilityOfA = countOfA / totalCount;
+	let probabilityOfT = countOfT / totalCount;
+	let probabilityOfG = countOfG / totalCount;
+	let probabilityOfC = countOfC / totalCount;
+	let entropy = -1 * (
 											(probabilityOfA === 0 ? 0 : (probabilityOfA * getLogOfXBase2(probabilityOfA))) +
 											(probabilityOfT === 0 ? 0 : (probabilityOfT * getLogOfXBase2(probabilityOfT))) +
 											(probabilityOfG === 0 ? 0 : (probabilityOfG * getLogOfXBase2(probabilityOfG))) +
@@ -136,25 +141,25 @@ MyController.prototype.getEntropy_ = function(countOfA, countOfT, countOfG, coun
 
 /**
  * Initiates the entropy analysis process.
- * 
+ *
  * @private
- * @param {string} fileName The name of the file to analyze.			 
- */							
-MyController.prototype.process_ = function (fileName) {
-		this.refreshEntropyGraph_();					
+ * @param {string} fileName The name of the file to analyze.
+ */
+MyController.prototype.process_ = function(fileName) {
+		this.refreshEntropyGraph_();
 		this.intervalPromise_ = this.intervalService_((function() {
 			this.httpService_.get('http://localhost:8080/analyze')
-					 .then ((function (response) {
+					 .then((function(response) {
 							this.entropyOfCurrentWindow =
 									this.getEntropy_(
 											response.data.countObj.countOfA - this.countOfA,
 											response.data.countObj.countOfT - this.countOfT,
 											response.data.countObj.countOfG - this.countOfG,
-											response.data.countObj.countOfC - this.countOfC);									 
+											response.data.countObj.countOfC - this.countOfC);
 							this.countOfA = response.data.countObj.countOfA;
 							this.countOfT = response.data.countObj.countOfT;
 							this.countOfG = response.data.countObj.countOfG;
-							this.countOfC = response.data.countObj.countOfC;		
+							this.countOfC = response.data.countObj.countOfC;
 						if (response.data.statusCode === 'x') {
 								this.showEntropy = true;
 								this.entropy =
@@ -163,16 +168,16 @@ MyController.prototype.process_ = function (fileName) {
 								this.intervalService_.cancel(this.pieChartIntervalPromise_);
 							}
 					 }).bind(this), function(error) {});
-		}).bind(this), 10);					
+		}).bind(this), 10);
 };
 
 
 /**
- * Reset the variables in the tool to provide ability to the users to 
+ * Reset the variables in the tool to provide ability to the users to
  * start another task by halting an already-running task.
- * 
+ *
  * @private
- */						 
+ */
 MyController.prototype.reset_ = function() {
 		// Variables required for analyzing the entropy starts here.
 		this.countOfA = 0;
@@ -180,45 +185,63 @@ MyController.prototype.reset_ = function() {
 		this.countOfG = 0;
 		this.countOfC = 0;
 		this.showAnalysis = false;
-		this.showEntropy = false;	
-		this.entropyOfCurrentWindow = 0; 						
+		this.showEntropy = false;
+		this.entropyOfCurrentWindow = 0;
 		// Variables required for analyzing the entropy ends here.
 		
 		// Variables required for format conversion starts here.
 		this.startConversionToFasta = false;
 		this.startConversionToFastq = false;
 		// Variables required for format conversion ends here.
-}
+		
+		// Variables required for alpha-diversity computation starts here.
+		this.shouldShowSamples = false;
+};
 
 
 /**
- * Sends a HTTP request to download the required MetaSum data from
+ * Sends a HTTP request to download the required Metasub data from
  * http://ala.boku.ac.at/ server.
- */						 
+ */
 MyController.prototype.downloadMetaSubData = function() {
-		alert("Enter the below details to download the required file-\nUsername- 'CAMDA', Password- 'Pivo'");
+		alert('Enter the below details to download the required file-\nUsername- \'CAMDA\', Password- \'Pivo\'');
 		window.open(this.selectedCityUrlPrefix + this.accessionNumber + '.fastq.dsrc');
 };
 
 
+/**
+ * Shows all the samples uploaded by the user having the input API Key.
+ */
+MyController.prototype.showSamples = function() {
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://app.onecodex.com/api/v1/samples', true);
+		request.setRequestHeader('Authorization', 'Basic ' +  btoa(this.apiKey + ':'));
+		request.onload = (function () {
+			this.samples = JSON.parse(request.responseText);
+			this.shouldShowSamples = true;
+			this.scope_.$digest();
+		}).bind(this);
+		request.send();
+};
+
 
 angular
 				.module('myApp', [])
-				.controller('MyController', ['$document', '$http', '$interval', MyController])
+				.controller('MyController', ['$scope', '$document', '$http', '$interval', MyController])
 				.directive('filePicker', function($http, $interval) {
-						return  {
+						return {
 							link: function(scope, elem, attr, ctrl) {
 								elem.on('change', function() {
 									scope.myCtrl.reset_();
-									var fileName = elem[0].files[0].name;
+									let fileName = elem[0].files[0].name;
 									scope.myCtrl.fileName = fileName;
-									switch(elem[0].id) {
+									switch (elem[0].id) {
 										case 'fileInputAnalyze':
-											if (isFileHavingCorrectFormat(fileName, ".fastq") === false &&
-													isFileHavingCorrectFormat(fileName, ".fasta") === false) {
+											if (isFileHavingCorrectFormat(fileName, '.fastq') === false &&
+													isFileHavingCorrectFormat(fileName, '.fasta') === false) {
 												alert('Please upload a FASTQ/FASTA file');
 												break;
-											}													
+											}
 											scope.myCtrl.showAnalysis = true;
 											scope.$apply();	// Placing scope.$apply() to update the view even if removing it works fine.
 											$interval.cancel(scope.myCtrl.intervalPromise_);
@@ -227,14 +250,14 @@ angular
 												'http://localhost:8080/analyze',
 												{name: fileName},
 												{headers: {'Content-Type': 'application/json', 'Authorization': 'Basic '}})
-												.then(function() {	
+												.then(function() {
 													scope.myCtrl.process_(fileName);
 												}, function(error) {})
 												.catch(function() {});
 												break;
 										case 'fileInputConvertToFasta':
 											scope.$apply();
-											if (isFileHavingCorrectFormat(fileName, ".fastq") === false) {
+											if (isFileHavingCorrectFormat(fileName, '.fastq') === false) {
 												alert('Please upload a FASTQ file');
 												break;
 											}
@@ -250,9 +273,9 @@ angular
 												}, function(error) {})
 												.catch(function() {});
 											break;
-										case 'fastaFileUploader':															
+										case 'fastaFileUploader':
 											scope.$apply();
-											if (isFileHavingCorrectFormat(fileName, ".fasta") === false) {
+											if (isFileHavingCorrectFormat(fileName, '.fasta') === false) {
 												alert('Please upload a FASTA file');
 												break;
 											}
@@ -265,19 +288,19 @@ angular
 													'http://localhost:8080/convert-to-fastq',
 													{
 														fastaFileName: scope.myCtrl.fastaFileName,
-														qualFileName: scope.myCtrl.qualFileName
+														qualFileName: scope.myCtrl.qualFileName,
 													},
 													{headers: {'Content-Type': 'application/json', 'Authorization': 'Basic '}})
 													.then(function(response) {
-														download(scope.myCtrl.fastaFileName.substring(0, scope.myCtrl.fastaFileName.length-6) + '.fastq', response.data);				
+														download(scope.myCtrl.fastaFileName.substring(0, scope.myCtrl.fastaFileName.length-6) + '.fastq', response.data);
 														scope.myCtrl.startConversionToFastq = false;
 													}, function(error) {})
 													.catch(function() {});
 											}
 											break;
-										case 'qualFileUploader':															
+										case 'qualFileUploader':
 											scope.$apply();
-											if (isFileHavingCorrectFormat(fileName, ".qual") === false) {
+											if (isFileHavingCorrectFormat(fileName, '.qual') === false) {
 												alert('Please upload a QUAL file');
 												break;
 											}
@@ -290,18 +313,18 @@ angular
 													'http://localhost:8080/convert-to-fastq',
 													{
 														fastaFileName: scope.myCtrl.fastaFileName,
-														qualFileName: scope.myCtrl.qualFileName
+														qualFileName: scope.myCtrl.qualFileName,
 													},
 													{headers: {'Content-Type': 'application/json', 'Authorization': 'Basic '}})
 													.then(function(response) {
-														download(scope.myCtrl.fastaFileName.substring(0, scope.myCtrl.fastaFileName.length-6) + '.fastq', response.data);				
+														download(scope.myCtrl.fastaFileName.substring(0, scope.myCtrl.fastaFileName.length-6) + '.fastq', response.data);
 														scope.myCtrl.startConversionToFastq = false;
 													}, function(error) {})
 													.catch(function() {});
 											}
-											break;																													
+											break;
 									}
 								});
-							}
+							},
 						};
 				});
