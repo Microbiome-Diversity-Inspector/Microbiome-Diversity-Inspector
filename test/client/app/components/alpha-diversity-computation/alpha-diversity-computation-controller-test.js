c9cf8177c8d4f440fb47023399af777e1263be50
Rachit Belwariar <gbelwariar@gmail.com>
2018-02-23 00:49:09 +0530

    Add unit-tests for testing the following methods on controller-
    a) Resetting the alpha-diversity computation variables.
    b) Should compute the mean and sample-deviation of the selected samples for the below scenarios - 
        i) If no sample is selected.
       ii) If one of the selected sample's alpha-diversity is not computed yet by the server.

--- test/client/app/components/alpha-diversity-computation/alpha-diversity-computation-controller-test.js
+++ test/client/app/components/alpha-diversity-computation/alpha-diversity-computation-controller-test.js
@@ -1,1 +1,69 @@
-// Unit test 'alphaDiversityComputationCtrl'.
+describe('AlphaDiversityComputationCtrl', function() {
+	
+	beforeEach(angular.mock.module('microbiomeDiversityInspector'));
+	
+	var ctrl;
+	var scope;
+	var createSample = function(
+			primaryClassificationRef,
+			orderOfDiversity,
+			alphaDiversity,
+			alphaDiversityComputationStarted,
+			alphaDiversityComputationCompleted,
+			isIncluded) {
+				return {
+					primary_classification: {
+						$ref: primaryClassificationRef
+					},
+					orderOfDiversity: orderOfDiversity,
+					alphaDiversity: alphaDiversity,
+					alphaDiversityComputationStatus: {
+						started: alphaDiversityComputationStarted,
+						completed: alphaDiversityComputationCompleted
+					},
+					isIncluded: {
+						value: isIncluded
+					}
+				};
+			};
+	
+	beforeEach(inject(function(_$rootScope_, _$controller_) {
+		scope = _$rootScope_.$new();
+		ctrl = _$controller_('AlphaDiversityComputationCtrl', {$scope: scope});
+	}));
+	
+	it('should reset alpha diversity computation variables', function() {
+		ctrl.resetAlphaDiversityComputationVariables_();
+		expect(ctrl.shouldShowSamples).toBe(false);
+		expect(ctrl.shouldShowLoaderWhileRetrievingSamples).toBe(false);
+		expect(ctrl.resultantMeanAndStandardDeviation).toEqual({
+			mean: '_',
+			standardDeviation: '_'
+		});
+	});
+	
+	describe('when computing mean and standard deviation in alpha diversity', function() {
+		
+		it('should show \'_\' if no sample is selected', function() {
+			ctrl.samples = [];
+			ctrl.computeMeanAndStandardDeviationOfSelectedSamples_();
+			expect(ctrl.resultantMeanAndStandardDeviation).toEqual({
+				mean: '_',
+				standardDeviation: '_'
+			});
+		});
+		
+		it('should show \'_\' if the selected sample\'s alpha-diversity is not computed yet by the server',
+			function() {
+				ctrl.samples = [
+					createSample(undefined, undefined, '_', undefined, undefined, true),
+					createSample(undefined, undefined, '3.4', undefined, undefined, true)
+				];
+				ctrl.computeMeanAndStandardDeviationOfSelectedSamples_();
+				expect(ctrl.resultantMeanAndStandardDeviation).toEqual({
+					mean: '_',
+					standardDeviation: '_'
+				});
+		});		
+	});
+});
