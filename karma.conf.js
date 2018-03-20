0a46bba49140165555f7b2fd1801182e015c2dc6
Rachit Belwariar <gbelwariar@gmail.com>
2018-03-20 20:28:55 +0530

    Use 'browserify' to provide Karma with functions like - 'require' so that the client-side tests can be run in browser.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
@@ -33,6 +33,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+			'test/**/*.js': [ 'browserify' ]
     },
 
 
