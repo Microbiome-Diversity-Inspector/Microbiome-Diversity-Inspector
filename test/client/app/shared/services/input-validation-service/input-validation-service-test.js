2fcda6f2168d1fe86c8828faaeda20dd8baa2c91
Rachit Belwariar <gbelwariar@gmail.com>
2018-03-20 23:43:13 +0530

    Unit test 'InputValidationService' service.

--- test/client/app/shared/services/input-validation-service/input-validation-service-test.js
+++ test/client/app/shared/services/input-validation-service/input-validation-service-test.js
@@ -1,1 +1,28 @@
-// Unit test 'input-validation-service'.
+describe('InputValidationService', function() {
+	
+	beforeEach(angular.mock.module('microbiomeDiversityInspector'));
+	
+	let inputValidationService;
+	
+	beforeEach(inject(function(_inputValidationService_) {
+		inputValidationService = _inputValidationService_;
+	}));
+	
+	it('should verify if a given file is of the given file format', function() {
+		expect(inputValidationService.isFileHavingCorrectFormat('sample.fastq', 'fastq')).toBe(true);
+		expect(inputValidationService.isFileHavingCorrectFormat('sample.fastq', 'fasta')).toBe(false);
+	});
+	
+	it('should remove leading and trailing whitespaces from the given string', function() {
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('')).toBe('');	
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('sample')).toBe('sample');
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('    sample  '))
+				.toBe('sample');
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('sample    '))
+				.toBe('sample');
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('   sample'))
+				.toBe('sample');				
+		expect(inputValidationService.removeLeadingAndTrailingWhitespaces('   sam   ple    '))
+				.toBe('sam   ple');				
+	});
+});
