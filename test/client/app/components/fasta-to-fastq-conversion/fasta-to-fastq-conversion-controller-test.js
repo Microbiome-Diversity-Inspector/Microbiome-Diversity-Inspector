3e404cc6726ca294853bee6c7bf28cb24804d802
Rachit Belwariar <gbelwariar@gmail.com>
2018-03-21 20:28:31 +0530

    Unit test 'FastaToFastqConversionCtrl'.

--- test/client/app/components/fasta-to-fastq-conversion/fasta-to-fastq-conversion-controller-test.js
+++ test/client/app/components/fasta-to-fastq-conversion/fasta-to-fastq-conversion-controller-test.js
@@ -1,1 +1,21 @@
-// Unit test 'fastaToFastqConversionCtrl'.
+describe('Unit tests for FastaToFastqConversionCtrl', function() {
+		
+	beforeEach(angular.mock.module('microbiomeDiversityInspector'));
+	
+	let ctrl;
+	
+	beforeEach(inject(function(_$controller_) {
+		ctrl = _$controller_('FastaToFastqConversionCtrl', {});
+	}));
+	
+	it('should set initialize controller variables', function() {
+		spyOn(ctrl, 'resetFastaToFastqConversionVariables_');
+		ctrl.$onInit();		
+		expect(ctrl.resetFastaToFastqConversionVariables_).toHaveBeenCalled();
+	})
+	
+	it('should reset fasta to fastq conversion variables', function() {
+		ctrl.resetFastaToFastqConversionVariables_();
+		expect(ctrl.startConversionToFastq).toBe(false);
+	});
+});
